6d733cb8b290f714511f2c08330e08087d8b9a4e
Ameil <ameilburke115@gmail.com>
2024-12-04 16:08:32 +1300

    deleted:    dist/alert-details/entities/alert-detail.entity.d.ts 	deleted:    dist/alert-details/entities/alert-detail.entity.js 	deleted:    dist/alert-details/entities/alert-detail.entity.js.map 	modified:   dist/tsconfig.build.tsbuildinfo

--- dist/alert-details/entities/alert-detail.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AlertDetail = void 0;
-class AlertDetail {
-}
-exports.AlertDetail = AlertDetail;
-//# sourceMappingURL=alert-detail.entity.js.map
